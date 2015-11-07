fd40d0527c5c7c3072ff73ee819c6719ed1aac99
Christian Spreng <c.spreng@zoho.com>
2015-11-07 15:00:02 +0100

    updates

--- playground/gyro/main.c
+++ playground/gyro/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
-
-void main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+int main(void)
 {
-	printf("Hallo Welt\n");
+	printf("Starting to open I2C Gyro\n");
+	int fd;
+	if((fd=open(expander.I2CBus, O_RDWR)) < 0)
+	{
+		perror("Failt to open i2c bus\n");
+		exit(1);
+	}
+	if(ioctl(fd,I2C_SLAVE, expander.adress) < 0 )
+	{
+		perror("Failed to acquire bus access and/or talk to slave\n");
+	}
+
 }
