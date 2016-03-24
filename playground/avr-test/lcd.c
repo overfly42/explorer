5188d40252c2f863adfe637d7377302baf103e1c
Christian Spreng <c.spreng@zoho.com>
2016-03-24 06:31:30 +0000

    Minimal working example

--- playground/avr-test/lcd.c
+++ playground/avr-test/lcd.c
@@ -2,18 +2,20 @@
 #define F_CPU 1000000UL
 #include <util/delay.h>
 
-void setParallel(uint8_t data);
+//LCD
+#define P_ENABLE 1
+#define P_REG_SELECT 2
+#define P_READWRITE 4
+#define S_ENABLE 0
+#define S_REG_SELECT 1
+#define S_READWRITE 2
+#define DATA_REG 1
+#define FUNC_REG 0
+#define DATA_READ 1
+#define DATA_WRITE 0
 
-int main(void)
-{
-	DDRD = 0xFF;//Set all PORT D pins to Output
-	uint8_t i = 0;
-	while(1)
-	{
-		setParallel(i++);
-		_delay_ms(100);
-	}
-}
+
+//Shift Register
 #define SHCP 	1
 #define STCP 	2
 #define OE 		4
@@ -26,6 +28,29 @@ int main(void)
 #define SET_OUTPUT_2 (MR | STCP)
 #define INPUT_PREPAIR MR
 #define INPUT_WRITE (MR | SHCP)
+
+void setParallel(uint8_t data);
+void setLetter(uint8_t data);
+void init(void);
+void writeData(void);
+uint8_t letter[16];//16 TestLetter for LCD 
+uint8_t bitMask(uint8_t pos, uint8_t val);
+
+int main(void)
+{
+	DDRD = 0xFF;//Set all PORT D pins to Output for LEDs
+	DDRB = 0xFF;//Set all PORT B pins to Output for LCD
+	uint8_t i = 0;
+	i=0;
+	_delay_ms(100);//Display has to wait for more than 15ms
+	init();
+	while(1)
+	{
+	//	setParallel(i++);
+		setLetter(letter[((++i)&0x0F)]);
+		_delay_ms(1000);
+	}
+}
 void setParallel(uint8_t data)
 {
 	//PD0 --> SHCP
@@ -49,4 +74,62 @@ void setParallel(uint8_t data)
 	PORTD = SET_OUTPUT_1;//prepair
 		_delay_ms(100);
 	PORTD = SET_OUTPUT_2;//Raising Edge
+}
+
+void setLetter(uint8_t data)
+{
+	PORTB |= P_REG_SELECT; //Data reg is 1
+	//Remove the enable
+	setParallel(data);//Setting via Parallel Output
+	writeData();
+}
+void init (void)
+{
+	uint8_t i = 0;
+	//setting the letters
+	letter[i++] = 0b01000001;
+	letter[i++] = 0b01000010;
+	letter[i++] = 0b01000011;
+	letter[i++] = 0b01000100;
+	letter[i++] = 0b01000101;
+	letter[i++] = 0b01000110;
+	letter[i++] = 0b01000111;
+	letter[i++] = 0b01001000;
+	letter[i++] = 0b01001001;
+	letter[i++] = 0b01001010;
+	letter[i++] = 0b01001011;
+	letter[i++] = 0b01001100;
+	letter[i++] = 0b01001101;
+	letter[i++] = 0b01001110;
+	letter[i++] = 0b01001111;
+	letter[i++] = 0b01010000;
+	//Init the Display
+/* Works more or less
+	//Set Display Control to defined Values
+	PORTB = 0;//There should nothing active on PORT B on this time
+	setParallel(32 | 16 | 8);//See Page 44	
+	writeData();
+	setParallel(8 | 4 | 2);
+	writeData();
+	setParallel(4 | 2 );
+	writeData();
+	PORTB |= P_REG_SELECT;	
+	//Clear Display
+	setParallel(1);
+	writeData();
+*/	
+}
+uint8_t bitMask(uint8_t pos, uint8_t val)
+{
+	uint8_t data = 0;
+	uint8_t i;
+	for(i = 0;i<8;i++)//8 Bit
+		data |= (i == pos ? val : 1) << i;
+	return data;
+}
+void writeData(void)
+{
+		PORTB |= P_ENABLE;//Enable Write to Display
+		_delay_ms(10);
+		PORTB &= 0xFE;//First disable	
 }
